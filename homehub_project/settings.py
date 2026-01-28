b496db49497dfbcb9e29af69352291698cadcef0
Anton Carlsson <anton.carlsson@bdo.se>
2026-01-28 15:15:05 +0100

    Fix DEBUG and ALLOWED_HOSTS environment configuration

--- homehub_project/settings.py
+++ homehub_project/settings.py
@@ -21,9 +21,12 @@ SECRET_KEY = os.getenv(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "127.0.0.1,localhost"
+).split(",")
 
 
 # Application definition
