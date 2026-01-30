99d2d10fc144890ceb56ba36ad9b69989cf90863
Anton Carlsson <anton.carlsson@bdo.se>
2026-01-30 16:54:29 +0100

    Add ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS for Heroku

--- homehub_project/settings.py
+++ homehub_project/settings.py
@@ -25,10 +25,9 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.getenv(
-    "ALLOWED_HOSTS",
-    "127.0.0.1,localhost"
-).split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = [h.strip() for h in ALLOWED_HOSTS if h.strip()]
+
 
 
 # Application definition
@@ -122,6 +121,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
+CSRF_TRUSTED_ORIGINS = [o.strip() for o in CSRF_TRUSTED_ORIGINS if o.strip()]
 
 
 # Static files (CSS, JavaScript, Images)
